a6628621ac0972ad8ba42a095cedbc927f7ff288
MickaelFP <72392674+MickaelFP@users.noreply.github.com>
2021-01-27 18:31:33 +0100

    Update TableauM.js

--- src/scenes/TableauM.js
+++ src/scenes/TableauM.js
@@ -44,7 +44,7 @@ class TableauM extends Tableau{
         this.physics.add.overlap(this.player, this.monstre, this.hitSpike, null, this);
 
         this.monstre=this.physics.add.sprite(300,this.sys.canvas.height-70,"monster-violet");
-        this.monstre.setOrigin(400,100);
+        this.monstre.setOrigin(400,60);
         this.monstre.setDisplaySize(40,40);
         this.monstre.setCollideWorldBounds(true);
         this.monstre.setBounce(1);
@@ -55,11 +55,11 @@ class TableauM extends Tableau{
         this.monstre.setOrigin(0,0);
         this.monstre.setDisplaySize(64,64);
         this.monstre.setCollideWorldBounds(true);
-        this.monstre.setBounce(1);
-        this.monstre.setVelocityX(50);
+        this.monstre.setBounce(0);
+        this.monstre.setVelocityX(100);
         this.physics.add.overlap(this.player, this.monstre, this.hitSpike, null, this);
 
-        new MonsterFly(this,400,100);
+        new MonsterFly(this,400,80);
         //new MonsterFly2(this,600,50);
         /*
         this.monstre=this.physics.add.sprite(300,this.sys.canvas.height-70,"monster-violet");
