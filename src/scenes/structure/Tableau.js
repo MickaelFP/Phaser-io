7cf6d25bf241ddf6d5b5889c0c6d8d4475de550b
MickaelFP <72392674+MickaelFP@users.noreply.github.com>
2021-02-08 16:09:31 +0100

    Update Tableau.js

--- src/scenes/structure/Tableau.js
+++ src/scenes/structure/Tableau.js
@@ -141,3 +141,94 @@ class Tableau extends Phaser.Scene{
  * @type {null|Tableau}
  */
 Tableau.current=null;
+
+
+
+
+/* DEBUT SAIGNE : ANNIMATION DESTRUCTION DE MONSTRE
+
+saigne(object,onComplete){
+        let me=this;
+        me.blood.visible=true;
+        me.blood.rotation = Phaser.Math.Between(0,6);
+        me.blood.x=object.x;
+        me.blood.y=object.y;
+        me.tweens.add({
+            targets:me.blood,
+            duration:200,
+            displayHeight:{
+                from:40,
+                to:70,
+            },
+            displayWidth:{
+                from:40,
+                to:70,
+            },
+            onComplete: function () {
+                me.blood.visible=false;
+                onComplete();
+            }
+        })
+    }
+
+*/
+
+/* DEBUT HITSPIKE : TUER LES MONSTRES
+
+hitSpike (player, spike)
+    {
+        this.physics.pause();
+        player.setTint(0xff0000);
+        player.anims.play('turn');
+        this.scene.restart();
+
+    }
+
+    /**
+     * Quand on touche un monstre
+     * si on le touche par en haut on le tue, sinon c'est lui qui nous tue
+     * @param {Player} player
+     * @param {Phaser.Physics.Arcade.Sprite} monster
+     */     /*
+    hitMonster(player, monster){
+        let me=this;
+        if(monster.isDead !== true){ //si notre monstre n'est pas déjà mort
+            if(
+                // si le player descend
+                player.body.velocity.y > 0
+                // et si le bas du player est plus haut que le monstre
+                && player.getBounds().bottom < monster.getBounds().top+30
+
+            ){
+                ui.gagne();
+                monster.isDead=true; //ok le monstre est mort
+                monster.visible=false;
+                this.saigne(monster,function(){
+                    //à la fin de la petite anim...ben il se passe rien :)
+                })
+                //notre joueur rebondit sur le monstre
+                player.directionY=500;
+            }else{
+                //le joueur est mort
+                if(!me.player.isDead){
+                    me.player.isDead=true;
+                    me.player.visible=false;
+                    //ça saigne...
+                    me.saigne(me.player,function(){
+                        //à la fin de la petite anim, on relance le jeu
+                        me.blood.visible=false;
+                        me.player.anims.play('turn');
+                        me.player.isDead=false;
+                        me.scene.restart();
+                    })
+
+                }
+
+
+            }
+        }
+
+    }
+
+
+*/  // FIN HITSPIKE : TUER LES MONSTRES
